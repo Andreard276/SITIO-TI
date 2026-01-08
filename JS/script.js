167c3fbeb5c3b0e578bb7c9e8bbab1fd1f0fe242
andrea <240453@utxicotepec.edu.mx>
2026-01-08 14:29:07 -0600

    crear funciones js y estilo del encabezado

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,33 @@ function saludar () {
     alert("Bienvenido a la Academia de T.I");
 
 }
+function suma(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado= valor1 + valor2;
+    alert("Resultado de la suma es:"+resultado);
+}
+function resta(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado= valor1 + valor2;
+    alert("Resultado de la resta es:"+resultado);
+
+
+}
+function multiplicacion(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado= valor1 * valor2;
+    alert("Resultado de la multiplicacion de"+valor1+ "y " +valor2+"es: "+resultado);
+
+
+}
+function divicion(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado= valor1 / valor2;
+    alert("Resultado de la divicion de" +valor1+ "y " +valor2+" es: "+resultado);
+
+
+}
